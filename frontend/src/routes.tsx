0752132979b01a3fa4b91b7c4eb26e510903688b
Yuri Almeida <yurialves2@gmail.com>
2020-03-26 00:23:13 -0300

    page profile

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Logon } from './pages/Logon';
+import { Profile } from './pages/Profile';
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
