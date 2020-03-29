6fe2bae000febd544963bebb8fe01a3ec8cc3099
Yuri Almeida <yurialves2@gmail.com>
2020-03-28 23:56:57 -0300

    added Request Validators

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,4 +1,54 @@
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const RequestValidators = {
+  Profile: celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+
+  StoreOngs: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.string().required().min(10),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2)
+    })
+  }),
+
+  DeleteIncident: celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required()
+    })
+  }),
+
+  Incidents: celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number()
+    })
+  }),
+
+  StoreIncident: celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      title: Joi.string().required(),
+      description: Joi.string().required(),
+      value: Joi.number().required()
+    }),
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required()
+    }).unknown()
+  }),
+
+  Session: celebrate({
+    [Segments.BODY]: Joi.object({
+      id: Joi.string().required()
+    })
+  })
+}
+
 module.exports = {
+  RequestValidators,
   OngController: require('./OngController'),
   ProfileController: require('./ProfileController'),
   SessionController: require('./SessionController'),
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,20 +1,22 @@
 //@ts-check
 const { Router } = require('express');
 
+const { RequestValidators } = require('./controllers');
 const { OngController, ProfileController, IncidentController, SessionController } = require('./controllers');
+
 
 const routes = Router();
 
 routes.route('/ongs')
-    .get(OngController.index)
-    .post(OngController.store);
+  .get(OngController.index)
+  .post(RequestValidators.StoreOngs, OngController.store);
 
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', RequestValidators.Profile, ProfileController.index);
 
-routes.post('/sessions', SessionController.store);
+routes.post('/sessions', RequestValidators.Session, SessionController.store);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.store);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/incidents', RequestValidators.Incidents, IncidentController.index);
+routes.post('/incidents', RequestValidators.StoreIncident, IncidentController.store);
+routes.delete('/incidents/:id', RequestValidators.DeleteIncident, IncidentController.delete);
 
 module.exports = routes;
